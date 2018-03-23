49b80e08e07ac5f98dafd72a40642a6d1f59caf9
Tom Medema <tommedema@Toms-MacBook-Pro.local>
2018-03-23 13:31:52 +0100

    move container document margin:0 to fromDocument template

--- src/editor/config/config.js
+++ src/editor/config/config.js
@@ -25,6 +25,13 @@ module.exports = {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title></title>
+      <style data-gjs-from-doc>
+      body,
+      html {
+          height: 100%;
+          margin: 0;
+      }
+      </style>
     </head>
     <body></body>
   `,
